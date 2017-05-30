afac9b108d5de4b4ec7a674f75e541e2428ad22f
Juraci Vieira <juraci.vieira@gmail.com>
2017-05-30 16:16:29 -0300

    simple producer and consumer with docker

---
+++ consumer/index.js
@@ -0,0 +1,23 @@
+const Kafka = require('no-kafka');
+const brokers = "localhost:9092";
+
+
+const consumer = new Kafka.GroupConsumer({
+  connectionString: brokers,
+});
+
+const dataHandler = function (messageSet, topic, partition) {
+
+  return Promise.all(messageSet.map((m) => {
+    console.log('>>>> ', topic, partition, m.offset, m.message.value.toString('utf8'));
+    // commit offset
+    return consumer.commitOffset({topic: topic, partition: partition, offset: m.offset, metadata: 'optional'});
+  }));
+};
+
+const strategies = [{
+  subscriptions: ['darksouls'],
+  handler: dataHandler
+}];
+
+consumer.init(strategies);
